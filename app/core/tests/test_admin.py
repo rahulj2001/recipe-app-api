ee3c2c8a2f89a961df36760a239a8a4c312d5b22
Rahul Jaideep <rahul.jaideep@ibm.com>
2023-09-06 16:40:27 -0400

    perform linting

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
     """Tests for Django admin"""
 
@@ -43,5 +44,3 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-
-
