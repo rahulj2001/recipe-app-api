f9482da278ba948a12cb6f027bfe80b4bdf96d3e
Rahul Jaideep <rahul.jaideep@ibm.com>
2023-09-06 16:38:28 -0400

    setup django admin and swagger api base

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,47 @@
+"""
+Tests for models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successfull(self):
+        """Test creating a user with an email is successfull"""
+        email = 'a@example.com'
+        password = '12345'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['TEST1@Example.com', 'TEST1@example.com'],
+            ['Test1@EXAMPLE.COM', 'Test1@example.com'],
+            ['TEst1@example.COM', 'TEst1@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating a user without an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
